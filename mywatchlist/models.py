d7b0764ac5c8153591e57ecc01e6372fdf25e030
hayfa_najma <hayfanajmaa22@gmail.com>
2022-09-20 15:19:57 +0700

    selesai html, xml, json

---
+++ mywatchlist/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class MyWatchList(models.Model):
+    title = models.CharField(max_length=50)
+    release_date = models.CharField(max_length=50)
+    watched = models.CharField(max_length=50)
+    rating = models.IntegerField()
+    review = models.TextField()
