bd4a71f1a95a423b24dea55e448059f7d80f7c78
hayfa_najma <hayfanajmaa22@gmail.com>
2022-09-25 22:21:52 +0700

    selesai

--- todolist/models.py
+++ todolist/models.py
@@ -7,9 +7,3 @@ class Task(models.Model):
     date = models.DateField()
     title = models.CharField(max_length=50)
     description = models.TextField()
-    is_finished = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
-    def __str__(self):
-        return self.description
