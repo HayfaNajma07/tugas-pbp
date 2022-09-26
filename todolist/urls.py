30184ad281e7b70b4711249fff5b403709cf4a97
hayfa_najma <hayfanajmaa22@gmail.com>
2022-09-26 09:24:19 +0700

    selesai

--- todolist/urls.py
+++ todolist/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('create-task/', create_task, name='create_task'),
-    
 ]
