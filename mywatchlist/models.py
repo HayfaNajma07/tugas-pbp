5e651ea1420a1a9f4eecd2299793cf6b6e10e7c8
hayfa_najma <hayfanajmaa22@gmail.com>
2022-11-21 20:30:57 +0700

    update mywatchlist jadi bool tipe datanya :D

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class MyWatchList(models.Model):
     title = models.CharField(max_length=50)
     release_date = models.CharField(max_length=50)
-    watched = models.CharField(max_length=50)
+    watched = models.BooleanField(default=False)
     rating = models.IntegerField()
     review = models.TextField()
